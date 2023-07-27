66ad767a93ca839b6fd2075ef75a2ba655385fd7
Shinebayar04 <136776809+Shinebayar04@users.noreply.github.com>
2023-07-27 04:14:45 +0000

    update

---
+++ src/App/Count.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+const Count = () => {
+    const [count, setCount] = useState(0);
+    const onIncrement = () => {
+       let newCount= count +1 
+       // count = newCount
+       setCount(newCount)
+    }
+    const ondecrement = () => {
+       let newCount=  count -1
+       setCount(newCount)
+    }
+    const onreset = () => {
+        let newCount=  0
+       setCount(newCount)
+    }
+    return ( 
+        <div> <p>{count}</p>
+            <button onClick={onIncrement} className='inc'>Increment</button>
+            <button onClick={ondecrement} className='dec'>Decrement</button>
+            <button onClick={onreset} className='res'>Reset</button>
+        </div>
+     );
+}
+ 
+export default Count;
